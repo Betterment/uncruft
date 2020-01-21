f97c87783201830133bc7179d205171fa6cd72d3
smudge <nathan@ngriffith.com>
2020-01-21 15:17:38 -0500

    whitelist => record/ignorefile

--- spec/uncruft/deprecation_handler_spec.rb
+++ spec/uncruft/deprecation_handler_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe Uncruft::DeprecationHandler do
     let(:line_number) { 123 }
     let(:caller_label) { '<something>' }
     let(:message) { "Warning: BAD called from #{caller_label} at #{absolute_path}:#{line_number}" }
-    let(:expected_whitelist_entry) { 'Warning: BAD called from <something> at chicken/nuggets.rb' }
-    let(:expected_error) { "#{expected_whitelist_entry}:123" }
+    let(:expected_ignorefile_entry) { 'Warning: BAD called from <something> at chicken/nuggets.rb' }
+    let(:expected_error) { "#{expected_ignorefile_entry}:123" }
     let(:expected_error_message) do
       <<~ERROR.strip
         #{expected_error}
@@ -31,14 +31,14 @@ RSpec.describe Uncruft::DeprecationHandler do
       expect { subject.call(message, '') }.to raise_error(RuntimeError, expected_error_message)
     end
 
-    context 'when whitelisting new deprecations' do
+    context 'when recording new deprecations' do
       before do
-        allow(Uncruft).to receive(:whitelist_deprecations?).and_return(true)
+        allow(Uncruft).to receive(:record_deprecations?).and_return(true)
       end
 
       it 'sanitizes the message and writes it to the file' do
         expect { subject.call(message, '') }.to change { File.exist?(ignorefile_path) }.from(false).to(true)
-        expect(File.read(ignorefile_path)).to include(expected_whitelist_entry)
+        expect(File.read(ignorefile_path)).to include(expected_ignorefile_entry)
       end
 
       context 'when timecop is enabled' do
@@ -57,7 +57,7 @@ RSpec.describe Uncruft::DeprecationHandler do
 
     context 'when caller is an erb file' do
       let(:caller_label) { '_app_views_bananas_show__1234_567890' }
-      let(:expected_whitelist_entry) { 'Warning: BAD called from chicken/nuggets.rb' }
+      let(:expected_ignorefile_entry) { 'Warning: BAD called from chicken/nuggets.rb' }
 
       it 'sanitizes the message and raises an error' do
         expect { subject.call(message, '') }.to raise_error(RuntimeError, expected_error_message)
@@ -66,7 +66,7 @@ RSpec.describe Uncruft::DeprecationHandler do
 
     context 'when caller is "top (required)"' do
       let(:caller_label) { '<top (required)>' }
-      let(:expected_whitelist_entry) { 'Warning: BAD called from <global scope> at chicken/nuggets.rb' }
+      let(:expected_ignorefile_entry) { 'Warning: BAD called from <global scope> at chicken/nuggets.rb' }
 
       it 'sanitizes the caller and raises an error' do
         expect { subject.call(message, '') }.to raise_error(RuntimeError, expected_error_message)
@@ -75,7 +75,7 @@ RSpec.describe Uncruft::DeprecationHandler do
 
     context 'when caller is "main"' do
       let(:caller_label) { '<main>' }
-      let(:expected_whitelist_entry) { 'Warning: BAD called from <global scope> at chicken/nuggets.rb' }
+      let(:expected_ignorefile_entry) { 'Warning: BAD called from <global scope> at chicken/nuggets.rb' }
 
       it 'sanitizes the caller and raises an error' do
         expect { subject.call(message, '') }.to raise_error(RuntimeError, expected_error_message)
@@ -84,7 +84,7 @@ RSpec.describe Uncruft::DeprecationHandler do
 
     context 'when message includes custom gem path' do
       let(:absolute_path) { Pathname.new('/banana/banana/banana/gems/chicken/nuggets.rb') }
-      let(:expected_whitelist_entry) { "Warning: BAD called from <something> at $GEM_PATH/chicken/nuggets.rb" }
+      let(:expected_ignorefile_entry) { "Warning: BAD called from <something> at $GEM_PATH/chicken/nuggets.rb" }
 
       before do
         allow(ENV).to receive(:[]).and_call_original
@@ -122,14 +122,14 @@ RSpec.describe Uncruft::DeprecationHandler do
 
     context 'when caller is not a filepath' do
       let(:absolute_path) { '(pry)' }
-      let(:expected_whitelist_entry) { 'Warning: BAD called from <something> at (pry)' }
+      let(:expected_ignorefile_entry) { 'Warning: BAD called from <something> at (pry)' }
 
       it 'sanitizes the message and raises an error' do
         expect { subject.call(message, '') }.to raise_error(RuntimeError, expected_error_message)
       end
     end
 
-    context 'when whitelist exists' do
+    context 'when ignorefile exists' do
       let(:message) { "Warning: BAD called from #{absolute_path}:#{line_number}" }
       let(:file_content) do
         <<~IGNOREFILE
@@ -154,11 +154,11 @@ RSpec.describe Uncruft::DeprecationHandler do
         expect { subject.call(message, '') }.not_to change { File.read(ignorefile_path) }
       end
 
-      context 'when whitelisting new deprecations' do
+      context 'when recording new deprecations' do
         let(:line_number) { '456' }
 
         before do
-          allow(Uncruft).to receive(:whitelist_deprecations?).and_return(true)
+          allow(Uncruft).to receive(:record_deprecations?).and_return(true)
         end
 
         it 'does not raise an error and leaves the file intact' do
