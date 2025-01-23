9807d4cf5f4611d2dc0e4b8ff59b57242e671287
Irving Caro <irving.caro@betterment.com>
2025-01-23 14:18:36 -0600

    Fix: Add Rails.application.deprecators on Rails 7.1+
    https://github.com/rails/rails/pull/46049

--- spec/uncruft/railtie_spec.rb
+++ spec/uncruft/railtie_spec.rb
@@ -11,6 +11,18 @@ describe Uncruft::Railtie do
       .from(nil).to(a_collection_containing_exactly(an_instance_of(Uncruft::DeprecationHandler)))
   end
 
+  if Rails.gem_version > Gem::Version.new('7.1')
+    before do
+      allow(Uncruft).to receive(:record_deprecations?).and_return(true)
+    end
+
+    it 'includes deprecator into Rails.application.deprecators' do
+      app.initialize!
+
+      expect(app.deprecators[:uncruft]).to eq Uncruft.deprecator
+    end
+  end
+
   context 'when the configured behavior is :stderr' do
     before do
       Rails.application.config.active_support.deprecation = :stderr
